403881aba94852faf887eaa9dcde946140123718
Halvorson <34112@nelnet.net>
2021-06-23 09:19:35 -0500

    added code for assignment

---
+++ NumberManipulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((x * x) + 14 - 3);
+        }
+    }
+}
